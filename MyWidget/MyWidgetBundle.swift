b69b732def6c84264a83da07ea505271d3e7e7e2
Reales Myles <rclarence123@gmail.com>
2024-04-16 00:02:12 -0500

    Initial Commit

---
+++ MyWidget/MyWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  MyWidgetBundle.swift
+//  MyWidget
+//
+//  Created by Reales Rectoro Myles Clarence on 15/04/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MyWidget()
+        MyWidgetLiveActivity()
+    }
+}
---
+++ WidgetApp/WidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetAppApp.swift
+//  WidgetApp
+//
+//  Created by Reales Rectoro Myles Clarence on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
